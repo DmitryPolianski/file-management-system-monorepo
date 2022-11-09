f0a84b7bb5952af354fdb59287b21b36e3fa2260
Dmitry Polianski <dmitry.polianski@gmail.com>
2022-11-09 13:14:39 +0300

    Init User service

---
+++ user-service/src/test/groovy/com/file/management/system/user/UserServiceApplicationUnitSpec.groovy
@@ -0,0 +1,15 @@
+package com.file.management.system.user
+
+import spock.lang.Specification
+
+class UserServiceApplicationUnitSpec extends Specification {
+
+    def 'default test'() {
+        given: 'test resources'
+
+        when: 'something happens'
+
+        then: 'check result'
+    }
+
+}
